1f5a2e5127fdb39a641958116dfc42b735a11c47
NaynaBisht <nayna.22bce8037@vitapstudent.ac.in>
2025-12-01 00:54:07 +0530

    Added get mapping for flight number in Flight Controller

--- flight-service/src/main/java/com/flight/controller/FlightSearchController.java
+++ flight-service/src/main/java/com/flight/controller/FlightSearchController.java
@@ -2,11 +2,11 @@ package com.flight.controller;
 
 import java.util.List;
 
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import com.flight.entity.Flight;
 import com.flight.request.FlightSearchRequest;
 import com.flight.response.FlightSearchResponse;
 import com.flight.service.FlightService;
@@ -32,9 +32,15 @@ public class FlightSearchController {
 
 		return flightService.searchFlights(request).map(ResponseEntity::ok)
 				.switchIfEmpty(Mono.just(ResponseEntity.status(HttpStatus.NOT_FOUND)
-						.body(new FlightSearchResponse(0, List.of(), "No flights found for selected date and route"))))
-				.onErrorResume(ex -> Mono.just(
-						ResponseEntity.badRequest().body(new FlightSearchResponse(0, List.of(), ex.getMessage()))));
+						.body(new FlightSearchResponse(0, List.of(), "No flights found for selected date and route"))));
 	}
+	@GetMapping("/{flightNumber}")
+    public Mono<ResponseEntity<Flight>> getFlightByNumber(@PathVariable String flightNumber) {
+        log.info("Fetching details for flight number: {}", flightNumber);
+        
+        return flightService.getFlightByNumber(flightNumber)
+                .map(flight -> ResponseEntity.ok(flight))
+                .switchIfEmpty(Mono.just(ResponseEntity.status(HttpStatus.NOT_FOUND).build()));
+    }
 
 }
