748ce9b40def7ebf9081deff06de15876fa9a099
NaynaBisht <nayna.22bce8037@vitapstudent.ac.in>
2025-12-02 01:30:38 +0530

    Fixed Package names

--- api-gateway/src/test/java/com/apigateways/ApiGatewayApplicationTests.java
+++ api-gateway/src/test/java/com/apigateways/ApiGatewayApplicationTests.java
@@ -1,4 +1,4 @@
-package com.apiGateway;
+package com.apigateways;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
--- notification-service/src/test/java/com/notification/NotificationServiceApplicationTests.java
+++ notification-service/src/test/java/com/notification/NotificationServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.notificationService;
+package com.notification;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
