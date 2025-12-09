f1538759b302b236bc4985bb6a1099a5538f0dff
NaynaBisht <nayna.22bce8037@vitapstudent.ac.in>
2025-12-09 08:56:36 +0530

    User-service implementation

---
+++ user-service/src/main/java/com/user/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package com.user.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.user.models.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> { // ID is String
+  Optional<User> findByUsername(String username);
+
+  Boolean existsByUsername(String username);
+
+  Boolean existsByEmail(String email);
+}
