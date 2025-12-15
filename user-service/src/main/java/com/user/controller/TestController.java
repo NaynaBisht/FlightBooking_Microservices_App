1dd5bda8afddb8df4f90eff7058e275327610617
NaynaBisht <nayna.22bce8037@vitapstudent.ac.in>
2025-12-15 20:59:27 +0530

    remove admin authorization

--- user-service/src/main/java/com/user/controller/TestController.java
+++ user-service/src/main/java/com/user/controller/TestController.java
@@ -6,18 +6,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "*", maxAge = 3600)
+// @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/test")
 public class TestController {
-	
+
 	@GetMapping("/all")
 	public String allAccess() {
 		return "Public Content.";
 	}
 
 	@GetMapping("/user")
-	@PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
+	@PreAuthorize("hasRole('USER')")
 	public String userAccess() {
 		return "User Content.";
 	}
