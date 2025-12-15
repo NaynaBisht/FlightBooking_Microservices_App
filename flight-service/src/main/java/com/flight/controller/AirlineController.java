99d7a8242836afe512e0f6e8b141c190913e4044
NaynaBisht <nayna.22bce8037@vitapstudent.ac.in>
2025-12-15 20:57:47 +0530

    Validate Admins to add flights

--- flight-service/src/main/java/com/flight/controller/AirlineController.java
+++ flight-service/src/main/java/com/flight/controller/AirlineController.java
@@ -1,11 +1,13 @@
 package com.flight.controller;
 
 import org.springframework.http.HttpStatus;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 import com.flight.entity.Flight;
 import com.flight.request.AddFlightRequest;
@@ -25,11 +27,14 @@ public class AirlineController {
 	private final AirlineService airlineService;
 
 	@PostMapping("/inventory/add")
-	public Mono<ResponseEntity<Flight>> addFlight(@Valid @RequestBody AddFlightRequest request) {
+	public Mono<ResponseEntity<Flight>> addFlight(@Valid @RequestBody AddFlightRequest request,
+			@RequestHeader(value = "loggedInUserRole", required = false) String role) {
+		log.info("Request received from Role: " + role);
+		if (role == null || !role.contains("ADMIN")) {
+			return Mono.error(new RuntimeException("Access Denied: Admins Only"));
+		}
 		log.info("Adding flight: {}", request.getFlightNumber());
-
 		return airlineService.addFlight(request)
-				.flatMap(savedFlight ->
-                Mono.just(ResponseEntity.status(HttpStatus.CREATED).body(savedFlight)));
+				.flatMap(savedFlight -> Mono.just(ResponseEntity.status(HttpStatus.CREATED).body(savedFlight)));
 	}
 }
