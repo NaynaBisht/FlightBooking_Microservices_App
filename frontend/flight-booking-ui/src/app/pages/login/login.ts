c00aeb1e046c0ca0eb6b873e2d35bc004aa91138
NaynaBisht <nayna.22bce8037@vitapstudent.ac.in>
2025-12-15 20:50:20 +0530

    login page

---
+++ frontend/flight-booking-ui/src/app/pages/login/login.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { AuthService } from '../../core/auth/auth.service';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule, RouterModule],
+  templateUrl: './login.html'
+})
+export class LoginComponent {
+
+  loginForm!: FormGroup;
+  error = '';
+
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  login() {
+    if (this.loginForm.invalid) return;
+
+    this.authService.login(this.loginForm.value).subscribe({
+      next: (res) => {
+        this.authService.saveUser(res);
+        this.router.navigate(['/flights']);
+      },
+      error: () => {
+        this.error = 'Invalid username or password';
+      }
+    });
+  }
+}
