84bbb37e121cb91c49b988e3e72c8c292366580f
NaynaBisht <nayna.22bce8037@vitapstudent.ac.in>
2025-12-15 21:00:45 +0530

    added JsonProperty to match frontend request

--- user-service/src/main/java/com/user/payload/request/SignupRequest.java
+++ user-service/src/main/java/com/user/payload/request/SignupRequest.java
@@ -1,34 +1,42 @@
 package com.user.payload.request;
 
 import java.util.Set;
-
 import jakarta.validation.constraints.*;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class SignupRequest {
-    @NotBlank
-    @Size(min = 3, max = 20)
-    private String username;
 
-    @NotBlank
-    @Size(max = 50)
-    @Email
-    private String email;
+	@NotBlank
+	@Size(min = 3, max = 20)
+	private String username;
 
-    @NotBlank
-    private String firstName;
+	@NotBlank
+	@Size(max = 50)
+	@Email
+	private String email;
 
-    @NotBlank
-    private String lastName;
+	@NotBlank
+	@JsonProperty("firstName")
+	private String firstName;
 
-    @NotBlank
-    @Size(min = 10, max = 15) 
-    private String mobileNumber;
-    
-    private Set<String> role;
-    
-    @NotBlank
-    @Size(min = 6, max = 40)
-    private String password;
+	@NotBlank
+	@JsonProperty("lastName")
+	private String lastName;
+
+	@NotBlank
+	@Size(min = 10, max = 15)
+	@JsonProperty("mobileNumber")
+	private String mobileNumber;
+
+	private Set<String> role;
+
+	@NotBlank
+	@Size(min = 6, max = 40)
+	private String password;
 }
